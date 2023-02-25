c6fdd5d52e72102a0eab81f9572e6995856f29f2
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-02-26 00:05:09 +0100

    new commit

--- app/src/main/java/com/example/ajincodew/SplashActivity.java
+++ app/src/main/java/com/example/ajincodew/SplashActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class FirstActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +14,7 @@ public class FirstActivity extends AppCompatActivity {
         Thread thread=new Thread(){
             @Override
             public void run(){
-                Intent intent=new Intent(FirstActivity.this,MainActivity.class);
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
                 try {
                     sleep(3000);
                     startActivity(intent);
