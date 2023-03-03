9f4c38f48fae2b5ff0b50427ef97c48d084f752e
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-03-03 12:26:20 +0100

    new

--- app/src/main/java/com/example/ajincodew/LoginActivity.java
+++ app/src/main/java/com/example/ajincodew/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.ajincodew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
         this.password=findViewById(R.id.passLogin);
 
         login.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Toast.makeText(LoginActivity.this, "Login!", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/ajincodew/SignUpActivity.java
+++ app/src/main/java/com/example/ajincodew/SignUpActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,35 +35,27 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(SignUpActivity.this, "Welcome back!", Toast.LENGTH_SHORT).show();
+                saveUser();
+                /*Intent intent=new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(intent);*/
 
-                try {
-                    saveUser();
-                    Intent intent=new Intent(SignUpActivity.this,LoginActivity.class);
-                    startActivity(intent);
-
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
             }
         });
     }
 
-    public void saveUser() throws IOException {
-            String data=this.username+","+this.email+","+this.password+";\n";
-            OutputStream outputStream = null;
-            try {
-                outputStream = openFileOutput("db.txt", Context.MODE_APPEND);
-                outputStream.write(data.getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (outputStream != null) {
-                        outputStream.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    public void saveUser(){
+        // Créer un objet SharedPreferences
+            SharedPreferences sharedPreferences = getSharedPreferences("auth_preferences", MODE_PRIVATE);
+
+        // Créer un objet Editor
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // Stocker le login et le mot de passe dans les préférences
+            editor.putString("username", username.getText().toString());
+            editor.putString("email", email.getText().toString());
+            editor.putString("password", password.getText().toString());
+
+        // Enregistrer les modifications dans les préférences
+        editor.apply();
         }
-    }
 }
